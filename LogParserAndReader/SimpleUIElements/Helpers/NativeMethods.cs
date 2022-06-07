2595c02e931290d27acdace62b02b34c61621579
Benjamin <Benjamin.Robert.Isherwood@gmail.com>
2022-06-06 23:33:26 -0400

    Completed the custom caption bar
    - Added a native methods helper that imports required by the caption bar for moving the mouse and figuring out where the mouse is
    - Adjusted the caption bar size to correctly handle the buttons
    - Updated the caption bar to only do the parent look up once, it was doing a look up for the owner every time it needed access to the parent window
    - Updated the caption bar to keep track of the last known width so it can move the window to the mouse cursor on a drag restore
    - Adjusted the close button toolbar such that the buttons have a thin border on highlight

---
+++ LogParserAndReader/SimpleUIElements/Helpers/NativeMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SimpleUIElements.Helpers
+{
+    static class NativeMethods
+    {
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr WindowFromPoint(POINT point);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetParent(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(ref POINT lpPoint);
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int x;
+        public int y;
+
+        public POINT(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
--- LogParserAndReader/SimpleUIElements/Views/CustomWindowCaptionBar.xaml.cs
+++ LogParserAndReader/SimpleUIElements/Views/CustomWindowCaptionBar.xaml.cs
@@ -1,6 +1,6 @@
-﻿using SimpleUIElements.ViewModels;
+﻿using SimpleUIElements.Helpers;
+using SimpleUIElements.ViewModels;
 using System;
-using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -12,6 +12,21 @@ namespace SimpleUIElements.Views
     /// </summary>
     public partial class CustomWindowCaptionBar : UserControl
     {
+        private Window? _parentWindow;
+
+        public Window ParentWindow
+        {
+            get
+            {
+                if(_parentWindow == null)
+                {
+                    _parentWindow = Window.GetWindow(this);
+                }
+                return _parentWindow;
+            }
+        }
+
+        private double _lastValidWidth = 0;
         private readonly CustomWindowCaptionBarViewModel _viewModel = new();
         public CustomWindowCaptionBar()
         {
@@ -21,8 +36,7 @@ namespace SimpleUIElements.Views
 
         private void CustomWindowCaptionBar_StateChanged(object? sender, EventArgs e)
         {
-            var newState = Window.GetWindow(this).WindowState;
-            if (newState == WindowState.Maximized)
+            if (ParentWindow.WindowState == WindowState.Maximized)
             {
                 _viewModel.IsWindowMaximized = true;
             }
@@ -34,41 +48,59 @@ namespace SimpleUIElements.Views
 
         private void CustomWindowCaptionBar_Loaded(object sender, RoutedEventArgs e)
         {
-            Window.GetWindow(this).StateChanged += CustomWindowCaptionBar_StateChanged;
+            ParentWindow.StateChanged += CustomWindowCaptionBar_StateChanged;
+            ParentWindow.SizeChanged += CustomWindowCaptionBar_SizeChanged;
+            _lastValidWidth = ParentWindow.ActualWidth;
+        }
+
+        private void CustomWindowCaptionBar_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if(ParentWindow.WindowState != WindowState.Maximized)
+            {
+                _lastValidWidth = ParentWindow.ActualWidth;
+            }
         }
 
         private void CustomWindowCaptionBar_UnLoaded(object sender, RoutedEventArgs e)
         {
-            Window.GetWindow(this).StateChanged -= CustomWindowCaptionBar_StateChanged;
+            ParentWindow.StateChanged -= CustomWindowCaptionBar_StateChanged;
+            ParentWindow.SizeChanged -= CustomWindowCaptionBar_SizeChanged;
         }
 
         private void DragWindow(object sender, MouseButtonEventArgs e)
         {
             if(e.RightButton != MouseButtonState.Pressed)
             {
-                Window.GetWindow(this).WindowState = WindowState.Normal;
-                Window.GetWindow(this).DragMove();
+                if (ParentWindow.WindowState == WindowState.Maximized)
+                {
+                    POINT test = new();
+                    NativeMethods.GetCursorPos(ref test);
+                    ParentWindow.Top = test.y - 10;
+                    ParentWindow.Left = test.x - (_lastValidWidth / 2);
+                }
+                ParentWindow.WindowState = WindowState.Normal;
+                ParentWindow.DragMove();
             }
         }
 
         private void CloseWindowMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Window.GetWindow(this).Close();
+            ParentWindow.Close();
         }
 
         private void MinimizeWindowMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Window.GetWindow(this).WindowState = WindowState.Minimized;
+            ParentWindow.WindowState = WindowState.Minimized;
         }
 
         private void MaximizeWindowMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Window.GetWindow(this).WindowState = WindowState.Maximized;
+            ParentWindow.WindowState = WindowState.Maximized;
         }
 
         private void RestoreWindowMenuItem_Click(object sender, RoutedEventArgs e)
         {
-            Window.GetWindow(this).WindowState = WindowState.Normal;
+            ParentWindow.WindowState = WindowState.Normal;
         }
     }
 }
